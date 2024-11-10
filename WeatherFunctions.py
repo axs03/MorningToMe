0f2c2f8cb5b3035b8dfb9d877a8ca4bcf33b5b9d
Aman Sahu <144287512+as9219@users.noreply.github.com>
2024-11-10 00:07:17 -0500

    broken links

--- WeatherFunctions.py
+++ WeatherFunctions.py
@@ -1,37 +1,32 @@
-from datetime import datetime
 from geopy.geocoders import Nominatim
 import requests
 import math
 
 
-class WeatherFunctions:
+class WeatherFunctions():
+    def __init__(self):
     # used by geopy
-    locator = Nominatim(user_agent="GetLoc")
+        self.locator = Nominatim(user_agent="GetLoc")
 
-    @staticmethod
-    def kelvinToCelsius(kelvin):
+    def kelvinToCelsius(kelvin: float):
         return kelvin - 273.15
 
-    @staticmethod
-    def kelvinToFahrenheit(kelvin):
+    def kelvinToFahrenheit(kelvin: float):
         return (kelvin - 273.15) * (9 / 5) + 32
 
-    @staticmethod
-    def CtoF(celsius):
+
+    def CtoF(celsius: float):
         return math.ceil((celsius * 9 / 5) + 32)
 
-    @staticmethod
-    def getCurrentLocation(location):
-        getLocation = WeatherFunctions.locator.geocode(location)
-        print(getLocation.address)
-        print("Latitude = ", getLocation.latitude, "\n")
-        print("Longitude = ", getLocation.longitude)
+
+    def getCurrentLocation(self,location):
+        getLocation = self.locator.geocode(location)
         return {"long": getLocation.latitude, "lat": getLocation.longitude}
 
-    @staticmethod
+
     def getWeather():
         # extracting json from openWeather
-        dataframe = requests.get(keys.coord_url).json()
+        dataframe = requests.get(keys.coord_url).json() # fix this
         curr_weather = dataframe['weather'][0]['main']
         curr_temp = WeatherFunctions.CtoF(dataframe['main']['temp'])
         curr_low = WeatherFunctions.CtoF(dataframe['main']['temp_min'])
@@ -41,9 +36,8 @@ class WeatherFunctions:
         weather_dict = {"temp": str(curr_temp) + " F", "high": str(curr_high) + " F", "low": str(curr_low) + " F",
                         "weather": str(curr_weather) + " F"}
         return weather_dict
-        pass
 
-    @staticmethod
+
     def bringUmbrella(weather_dictionary):
         if weather_dictionary["weather"] == 'Rain':
             return "Bring it"
