7d85ab11d2dbc5eaf4a0a85a0609ad95788e2d86
Aman Sahu <144287512+as9219@users.noreply.github.com>
2024-11-12 13:08:18 -0500

    added logic to classes and added priv functions. Tried adding the weather stuff

--- WeatherFunctions.py
+++ WeatherFunctions.py
@@ -2,24 +2,30 @@ from geopy.geocoders import Nominatim
 import requests
 import math
 from dotenv import load_dotenv
+import os
 
 
 class WeatherFunctions():
     def __init__(self):
         load_dotenv()
 
-        # used by geopy
+        # locater is used by geopy
         self.locator = Nominatim(user_agent="GetLoc")
-        self.coord_url = "https://api.openweathermap.org/data/2.5/weather?lat={latitude}&lon={longitude}&appid={openWeatherAPI}&units=metric"
+
+        self.longitude = ""
+        self.latitude = ""
+        self.openWeatherAPI = os.getenv("openWeatherAPI")
+        self.coord_url = ""
 
     def kelvinToCelsius(kelvin: float) -> float:
         return kelvin - 273.15
+
 
     def kelvinToFahrenheit(kelvin: float) -> float:
         return (kelvin - 273.15) * (9 / 5) + 32
 
 
-    def CtoF(celsius: float) -> float:
+    def __CtoF(celsius: float) -> float:
         return math.ceil((celsius * 9 / 5) + 32)
 
 
@@ -27,14 +33,21 @@ class WeatherFunctions():
         getLocation = self.locator.geocode(location)
         return {"long": getLocation.latitude, "lat": getLocation.longitude}
 
+    def __setCurrentLocation(self, location_dcitionary):
+        self.longitude, self.latitude = location_dcitionary["long"], location_dcitionary["lat"]
+        
 
     def getWeather(self):
+        # setting the latitude and longitude for the reques to make
+        self.__setCurrentLocation(self.getCurrentLocation(os.getenv("curr_city")))
+        self.coord_url = "https://api.openweathermap.org/data/2.5/weather?lat={self.latitude}&lon={self.longitude}&appid={self.openWeatherAPI}&units=metric"
+
         # extracting json from openWeather
         dataframe = requests.get(self.coord_url).json()
         curr_weather = dataframe['weather'][0]['main']
-        curr_temp = WeatherFunctions.CtoF(dataframe['main']['temp'])
-        curr_low = WeatherFunctions.CtoF(dataframe['main']['temp_min'])
-        curr_high = WeatherFunctions.CtoF(dataframe['main']['temp_max'])
+        curr_temp = WeatherFunctions.__CtoF(dataframe['main']['temp'])
+        curr_low = WeatherFunctions.__CtoF(dataframe['main']['temp_min'])
+        curr_high = WeatherFunctions.__CtoF(dataframe['main']['temp_max'])
 
         # Dictionary for current weather
         weather_dict = {"temp": str(curr_temp) + " F", "high": str(curr_high) + " F", "low": str(curr_low) + " F",
@@ -43,5 +56,5 @@ class WeatherFunctions():
 
 
     def bringUmbrella(weather_dictionary):
-        return "Bring It "if weather_dictionary["weather"] == 'Rain' else "Don't Need It"
+        return "bring an Umbrella" if weather_dictionary["weather"] == 'Rain' else "don't bring an Umbrella"
         
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 from TimezoneFunctions import TimezoneFunctions
+from WeatherFunctions import WeatherFunctions
 from sms_function import SMS
 from dotenv import load_dotenv
 
@@ -7,12 +8,20 @@ from dotenv import load_dotenv
 if __name__ == "__main__":
     load_dotenv()
 
+    # Init of the classes and object
+    # TimezoneFunctions is not a class
+    weather_functions = WeatherFunctions()
+
     # Stuff for the Timzones and Dates
     times = TimezoneFunctions.convertTimeZone("America/New_York")
-    local_time, india_time = times['curr_time'], times['india_times']
+    local_time, india_time = times["curr_time"], times["india_time"]
     day = TimezoneFunctions.getCurrentDay()
 
     # Stuff for the Weather and Rain Probability
+    weather_dict = weather_functions.getWeather()
+    temperature = weather_dict["temp"]
+    weather = weather_dict["weather"]
+    need_umbrella = weather_functions.bringUmbrella()
 
 
     subject = """ {day} Report """
@@ -20,7 +29,12 @@ if __name__ == "__main__":
     sms_body = """
                 The current local time is {local_time}. \n 
                 The current time in India is {india_time}. \n
+                The current tempersture locally is {temperature}. \n
+                The current weather locally is {weather}. \n
+                So, {need_umbrella}.
     """
 
-    sms = SMS(os.getenv("from_email"), os.getenv("email_passwd"), os.getenv("to_email"), subject=subject, body=sms_body)
-    sms.newSMS()
+    print(subject, sms_body)
+
+    # sms = SMS(os.getenv("from_email"), os.getenv("email_passwd"), os.getenv("to_email"), subject=subject, body=sms_body)
+    # sms.newSMS()
